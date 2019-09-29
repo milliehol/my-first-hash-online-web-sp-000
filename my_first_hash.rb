80eef938f434cabcca87bed33477d4636dc05337
Carla Holcomb <carla.programmer@gmail.com>
2019-09-29 21:39:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,13 +40,13 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"] = "2"
 
   # add 4 gun powder to the shipping_manifest hash below
 
 
 
   # return the shipping_manifest hash below
-  shipping_manifest[]
+  shipping_manifest
 
 end
